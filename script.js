2616d02bbe296d58184711c6299a2214d8060a86
Ben Saacks <108644584+Blancher@users.noreply.github.com>
2023-04-04 11:10:49 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,20 +3,20 @@ document.getElementById('submit').addEventListener('click', () => {
   const mode = document.getElementById('select').value;
   const word = document.getElementById('word').value;
   const returned = document.getElementById('returned');
-  const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-  let newString = '';
-  let returnedArray = [];
+  const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const returnedArray = [];
   if (word === '' || number === '') {
     returnedArray.push('Please fill out all input fields before submitting.');
   }
-  if (number > 26) {
+  if (+number > 26) {
     returnedArray.push("The shift number can't be greater than 26.");
   }
-  if (number < 0) {
+  if (+number < 0) {
     returnedArray.push("The shift number can't be negative.");
   }
   if (word !== '' && number >= 0 && number <= 26) {
-    number = parseInt(number);
+    let newString = '';
+    number = +number;
     const index = num => mode === 'encrypt' ? alphabet[alphabet.indexOf(word[num].toUpperCase()) + number] : alphabet[alphabet.lastIndexOf(word[num].toUpperCase()) - number];
     for (let i=0; i < word.length; i++) {
       newString += alphabet.includes(word[i].toUpperCase()) ? index(i) : word[i];
